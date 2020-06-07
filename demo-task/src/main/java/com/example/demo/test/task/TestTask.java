3ddaa4788fd00b21b67df67e55dcfbc46e9cd574
Jiakun Xu <jiakunxu@gmail.com>
2020-06-07 13:22:41 +0800

    aliyun-log

--- demo-task/src/main/java/com/example/demo/test/task/TestTask.java
+++ demo-task/src/main/java/com/example/demo/test/task/TestTask.java
@@ -3,6 +3,8 @@ package com.example.demo.test.task;
 import com.alibaba.schedulerx.worker.domain.JobContext;
 import com.alibaba.schedulerx.worker.processor.JavaProcessor;
 import com.alibaba.schedulerx.worker.processor.ProcessResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,8 +13,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class TestTask extends JavaProcessor {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestTask.class);
+
     @Override
     public ProcessResult process(JobContext context) {
+        logger.error(String.valueOf(System.currentTimeMillis()));
         return new ProcessResult(true, "success");
     }
 
