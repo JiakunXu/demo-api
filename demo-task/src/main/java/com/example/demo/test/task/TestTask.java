db734b937f3a974f7c41ae96cd9ce33d1fce1182
Jiakun Xu <jiakunxu@gmail.com>
2020-06-08 01:17:33 +0800

    test

--- demo-task/src/main/java/com/example/demo/test/task/TestTask.java
+++ demo-task/src/main/java/com/example/demo/test/task/TestTask.java
@@ -1,10 +1,13 @@
 package com.example.demo.test.task;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.schedulerx.worker.domain.JobContext;
 import com.alibaba.schedulerx.worker.processor.JavaProcessor;
 import com.alibaba.schedulerx.worker.processor.ProcessResult;
+import com.example.demo.api.shop.IShopService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,10 +18,12 @@ public class TestTask extends JavaProcessor {
 
     private static final Logger logger = LoggerFactory.getLogger(TestTask.class);
 
+    @Autowired
+    private IShopService        openShopService;
+
     @Override
     public ProcessResult process(JobContext context) {
-        logger.error(String.valueOf(System.currentTimeMillis()));
-        return new ProcessResult(true, "success");
+        return new ProcessResult(true, JSON.toJSONString(openShopService.countShop()));
     }
 
 }
