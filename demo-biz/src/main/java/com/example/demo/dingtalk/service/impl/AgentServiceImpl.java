1e8504c9abf1cfaae0866a2324b4f785ca8f3e6a
Jiakun Xu <jiakunxu@gmail.com>
2020-09-28 10:28:42 +0800

    Update AgentServiceImpl.java

--- demo-biz/src/main/java/com/example/demo/dingtalk/service/impl/AgentServiceImpl.java
+++ demo-biz/src/main/java/com/example/demo/dingtalk/service/impl/AgentServiceImpl.java
@@ -1,24 +1,45 @@
 package com.example.demo.dingtalk.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.api.dingtalk.AgentService;
 import com.example.demo.api.dingtalk.bo.Agent;
 import com.example.demo.dingtalk.dao.AgentDao;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  * @author JiakunXu
  */
-@Service
+@Service("agentService0")
 public class AgentServiceImpl implements AgentService {
 
     private static final Logger logger = LoggerFactory.getLogger(AgentServiceImpl.class);
 
+    @Autowired
     private AgentDao            agentDao;
 
     @Override
     public Agent getAgent(String agentId) {
+        if (StringUtils.isBlank(agentId)) {
+            return null;
+        }
+
+        Agent agent = new Agent();
+        agent.setAgentId(agentId);
+
+        return getAgent(agent);
+    }
+
+    private Agent getAgent(Agent agent) {
+        try {
+            return agentDao.getAgent(agent);
+        } catch (Exception e) {
+            logger.error(JSON.toJSONString(agent), e);
+        }
+
         return null;
     }
 
