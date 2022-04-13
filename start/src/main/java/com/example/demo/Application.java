73ec6ae779e85fea22bb793e6cbfb8a70566d173
Jiakun Xu <jiakunxu@gmail.com>
2022-04-13 13:36:29 +0800

    rename

--- start/src/main/java/com/example/demo/Application.java
+++ start/src/main/java/com/example/demo/Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication()
-@ImportResource(locations = { "classpath:bean/*-*.xml" })
-@MapperScan("com.example.demo.*.dao")
+@ImportResource(locations = { "classpath:bean/*.xml" })
+@MapperScan("com.example.demo.*.dao.mapper")
 public class Application {
 
     public static void main(String[] args) {
