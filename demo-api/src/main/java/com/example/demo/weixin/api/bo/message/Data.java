5cee6793b25c08f01790b3d5058424b8b1cac9ec
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-04-18 15:20:25 +0800

    upgrade

--- demo-api/src/main/java/com/example/demo/weixin/api/bo/message/Data.java
+++ demo-api/src/main/java/com/example/demo/weixin/api/bo/message/Data.java
@@ -1,9 +1,9 @@
 package com.example.demo.weixin.api.bo.message;
 
-import java.io.Serializable;
-
 import lombok.Getter;
 import lombok.Setter;
+
+import java.io.Serializable;
 
 /**
  * 
@@ -21,5 +21,65 @@ public class Data implements Serializable {
     private Keyword2          keyword2;
 
     private Keyword3          keyword3;
+
+    @Getter
+    @Setter
+    public static class Keyword1 extends Value {
+
+        private static final long serialVersionUID = -3139515201876777315L;
+
+        public Keyword1() {
+        }
+
+        public Keyword1(String value) {
+            this.setValue(value);
+        }
+
+        public Keyword1(String value, String color) {
+            this.setValue(value);
+            this.setColor(color);
+        }
+
+    }
+
+    @Getter
+    @Setter
+    public static class Keyword2 extends Value {
+
+        private static final long serialVersionUID = -3139515201876777315L;
+
+        public Keyword2() {
+        }
+
+        public Keyword2(String value) {
+            this.setValue(value);
+        }
+
+        public Keyword2(String value, String color) {
+            this.setValue(value);
+            this.setColor(color);
+        }
+
+    }
+
+    @Getter
+    @Setter
+    public static class Keyword3 extends Value {
+
+        private static final long serialVersionUID = -3139515201876777315L;
+
+        public Keyword3() {
+        }
+
+        public Keyword3(String value) {
+            this.setValue(value);
+        }
+
+        public Keyword3(String value, String color) {
+            this.setValue(value);
+            this.setColor(color);
+        }
+
+    }
 
 }
