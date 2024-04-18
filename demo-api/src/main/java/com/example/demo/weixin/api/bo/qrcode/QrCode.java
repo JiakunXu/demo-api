337aa1fc278742c1cbafb1d3e72c308ceb2e09c2
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-04-18 15:39:10 +0800

    upgrade

--- demo-api/src/main/java/com/example/demo/weixin/api/bo/qrcode/QrCode.java
+++ demo-api/src/main/java/com/example/demo/weixin/api/bo/qrcode/QrCode.java
@@ -37,4 +37,34 @@ public class QrCode implements Serializable {
     @JSONField(name = "action_info")
     private ActionInfo        actionInfo;
 
+    @Getter
+    @Setter
+    public static class ActionInfo implements Serializable {
+
+        private static final long serialVersionUID = 3676011129584429955L;
+
+        private Scene             scene;
+
+        @Getter
+        @Setter
+        public static class Scene implements Serializable {
+
+            private static final long serialVersionUID = -8212451358776340371L;
+
+            /**
+             * 场景值ID，临时二维码时为32位非0整型，永久二维码时最大值为100000（目前参数只支持1--100000）.
+             */
+            @JSONField(name = "scene_id")
+            private Integer           sceneId;
+
+            /**
+             * 场景值ID（字符串形式的ID），字符串类型，长度限制为1到64.
+             */
+            @JSONField(name = "scene_str")
+            private String            sceneStr;
+
+        }
+
+    }
+
 }
