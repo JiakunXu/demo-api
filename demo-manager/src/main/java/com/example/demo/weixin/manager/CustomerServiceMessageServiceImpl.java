e787df95dc0cbed2fe89eab156460ae5256c6333
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-04-19 11:14:15 +0800

    upgrade

--- demo-manager/src/main/java/com/example/demo/weixin/manager/CustomerServiceMessageServiceImpl.java
+++ demo-manager/src/main/java/com/example/demo/weixin/manager/CustomerServiceMessageServiceImpl.java
@@ -18,7 +18,6 @@ import com.example.demo.weixin.api.bo.message.Typing;
 import com.example.demo.weixin.api.bo.message.Video;
 import com.example.demo.weixin.api.bo.message.Voice;
 import com.example.demo.weixin.api.bo.message.WxCard;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -66,18 +65,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, Text text) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (text == null || StringUtils.isBlank(text.getContent())) {
-            throw new RuntimeException("content cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("text");
@@ -88,18 +75,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, Image image) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (image == null || StringUtils.isBlank(image.getMediaId())) {
-            throw new RuntimeException("media_id cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("image");
@@ -110,18 +85,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, Voice voice) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (voice == null || StringUtils.isBlank(voice.getMediaId())) {
-            throw new RuntimeException("media_id cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("voice");
@@ -132,18 +95,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, Video video) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (video == null || StringUtils.isBlank(video.getMediaId())) {
-            throw new RuntimeException("media_id cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("video");
@@ -154,18 +105,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, Music music) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (music == null || StringUtils.isBlank(music.getTitle())) {
-            throw new RuntimeException("title cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("music");
@@ -176,18 +115,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, News news) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (news == null || news.getArticleList() == null) {
-            throw new RuntimeException("articles cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("news");
@@ -198,18 +125,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, MpNews mpNews) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (mpNews == null || StringUtils.isBlank(mpNews.getMediaId())) {
-            throw new RuntimeException("media_id cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("mpnews");
@@ -221,18 +136,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
     @Override
     public Result send(String accessToken, String toUser,
                        MpNewsArticle mpNewsArticle) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (mpNewsArticle == null || StringUtils.isBlank(mpNewsArticle.getArticleId())) {
-            throw new RuntimeException("article_id cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("mpnewsarticle");
@@ -243,18 +146,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, WxCard wxCard) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (wxCard == null || StringUtils.isBlank(wxCard.getCardId())) {
-            throw new RuntimeException("card_id cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("wxcard");
@@ -266,18 +157,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
     @Override
     public Result send(String accessToken, String toUser,
                        MiniProgramPage miniProgramPage) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (miniProgramPage == null || StringUtils.isBlank(miniProgramPage.getTitle())) {
-            throw new RuntimeException("title cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("miniprogrampage");
@@ -288,18 +167,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
 
     @Override
     public Result send(String accessToken, String toUser, Link link) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
-        if (link == null || StringUtils.isBlank(link.getTitle())) {
-            throw new RuntimeException("title cannot be null.");
-        }
-
         Custom custom = new Custom();
         custom.setToUser(toUser);
         custom.setMsgType("link");
@@ -311,14 +178,6 @@ public class CustomerServiceMessageServiceImpl implements CustomerServiceMessage
     @Override
     public BaseResult setTyping(String accessToken, String toUser,
                                 Typing typing) throws RuntimeException {
-        if (StringUtils.isBlank(accessToken)) {
-            throw new RuntimeException("access_token cannot be null.");
-        }
-
-        if (StringUtils.isBlank(toUser)) {
-            throw new RuntimeException("touser cannot be null.");
-        }
-
         if (typing == null) {
             throw new RuntimeException("typing cannot be null.");
         }
