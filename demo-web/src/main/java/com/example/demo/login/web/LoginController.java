42518b4a86e46ea147559255638da4bc747d2162
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-04-22 13:40:17 +0800

    upgrade

--- demo-web/src/main/java/com/example/demo/login/web/LoginController.java
+++ demo-web/src/main/java/com/example/demo/login/web/LoginController.java
@@ -1,20 +1,18 @@
 package com.example.demo.login.web;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.alibaba.fastjson2.JSONObject;
-
 import com.example.demo.framework.constant.Constants;
 import com.example.demo.framework.exception.ServiceException;
 import com.example.demo.framework.response.TokenResponse;
 import com.example.demo.framework.web.BaseController;
 import com.example.demo.login.api.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping(value = "/")
