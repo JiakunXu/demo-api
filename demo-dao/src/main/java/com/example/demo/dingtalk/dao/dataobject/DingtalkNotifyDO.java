5b632ca23742f2a89be2e93cb8b7266b68adf73c
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-04-22 21:28:29 +0800

    upgrade

---
+++ demo-dao/src/main/java/com/example/demo/dingtalk/dao/dataobject/DingtalkNotifyDO.java
@@ -0,0 +1,15 @@
+package com.example.demo.dingtalk.dao.dataobject;
+
+import com.example.demo.framework.dataobject.BaseDO;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DingtalkNotifyDO extends BaseDO {
+
+    private static final long serialVersionUID = 6706644591052897288L;
+
+}
---
+++ demo-dao/src/main/java/com/example/demo/dingtalk/dao/mapper/DingtalkNotifyMapper.java
@@ -0,0 +1,7 @@
+package com.example.demo.dingtalk.dao.mapper;
+
+import com.example.demo.dingtalk.dao.dataobject.DingtalkNotifyDO;
+import com.example.demo.framework.mapper.BaseMapper;
+
+public interface DingtalkNotifyMapper extends BaseMapper<DingtalkNotifyDO> {
+}
