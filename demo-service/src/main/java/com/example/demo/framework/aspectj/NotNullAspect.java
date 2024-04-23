c7355e793d1836324729a7369d5bd813baf0b56e
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-04-24 00:58:59 +0800

    upgrade

--- demo-service/src/main/java/com/example/demo/framework/aspectj/NotNullAspect.java
+++ demo-service/src/main/java/com/example/demo/framework/aspectj/NotNullAspect.java
@@ -1,8 +1,6 @@
 package com.example.demo.framework.aspectj;
 
 import com.example.demo.framework.annotation.NotNull;
-import com.example.demo.framework.constant.Constants;
-import com.example.demo.framework.exception.ServiceException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -24,7 +22,7 @@ public class NotNullAspect {
             for (Annotation annotation : annotations[i]) {
                 if (annotation instanceof NotNull) {
                     if (joinPoint.getArgs()[i] == null) {
-                        throw new ServiceException(Constants.INTERNAL_SERVER_ERROR, "参数信息不能为空");
+                        throw new RuntimeException("参数信息不能为空");
                     }
                 }
             }
