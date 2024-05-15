fa52cab22c54e42d48481c965c0a3c4fec85c4af
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-05-15 10:06:47 +0800

    upgrade

--- demo-api/src/main/java/com/example/demo/dingtalk/api/MessageService.java
+++ demo-api/src/main/java/com/example/demo/dingtalk/api/MessageService.java
@@ -4,6 +4,12 @@ import java.util.Map;
 
 public interface MessageService {
 
+    String HTTPS_SEND_URL = "https://oapi.dingtalk.com/topapi/message/corpconversation/asyncsend_v2";
+
     Map<String, String> notify(String signature, String timeStamp, String nonce, String encrypt);
+
+    void send(String accessToken, Long agentId, String useridList, String mediaId);
+
+    void send(String accessToken, Long agentId, String useridList, String title, String text);
 
 }
--- demo-manager/src/main/java/com/example/demo/dingtalk/manager/MessageServiceImpl.java
+++ demo-manager/src/main/java/com/example/demo/dingtalk/manager/MessageServiceImpl.java
@@ -2,7 +2,13 @@ package com.example.demo.dingtalk.manager;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiMessageCorpconversationAsyncsendV2Request;
+import com.dingtalk.api.response.OapiMessageCorpconversationAsyncsendV2Response;
 import com.example.demo.dingtalk.api.MessageService;
+import com.taobao.api.ApiException;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -70,6 +76,76 @@ public class MessageServiceImpl implements MessageService {
         // 审批终止（发起人撤销审批单）
         if ("terminate".equals(type)) {
 
+        }
+    }
+
+    @Override
+    public void send(String accessToken, Long agentId, String useridList, String mediaId) {
+        DingTalkClient client = new DefaultDingTalkClient(HTTPS_SEND_URL);
+
+        OapiMessageCorpconversationAsyncsendV2Request request = new OapiMessageCorpconversationAsyncsendV2Request();
+        request.setAgentId(agentId);
+        request.setUseridList(useridList);
+
+        OapiMessageCorpconversationAsyncsendV2Request.Msg msg = new OapiMessageCorpconversationAsyncsendV2Request.Msg();
+        msg.setMsgtype("file");
+        msg.setFile(new OapiMessageCorpconversationAsyncsendV2Request.File());
+        msg.getFile().setMediaId(mediaId);
+        request.setMsg(msg);
+
+        OapiMessageCorpconversationAsyncsendV2Response response;
+
+        try {
+            response = client.execute(request, accessToken);
+        } catch (ApiException e) {
+            logger.error(com.alibaba.fastjson.JSON.toJSONString(request), e);
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        if (response == null) {
+            throw new RuntimeException("response is null.");
+        }
+
+        String errCode = response.getErrorCode();
+
+        if (StringUtils.isNotBlank(errCode) && !"0".equals(errCode)) {
+            throw new RuntimeException(response.getErrmsg());
+        }
+    }
+
+    @Override
+    public void send(String accessToken, Long agentId, String useridList, String title,
+                     String text) {
+        DingTalkClient client = new DefaultDingTalkClient(HTTPS_SEND_URL);
+
+        OapiMessageCorpconversationAsyncsendV2Request request = new OapiMessageCorpconversationAsyncsendV2Request();
+        request.setAgentId(agentId);
+        request.setUseridList(useridList);
+
+        OapiMessageCorpconversationAsyncsendV2Request.Msg msg = new OapiMessageCorpconversationAsyncsendV2Request.Msg();
+        msg.setMsgtype("markdown");
+        msg.setMarkdown(new OapiMessageCorpconversationAsyncsendV2Request.Markdown());
+        msg.getMarkdown().setText(text);
+        msg.getMarkdown().setTitle(title);
+        request.setMsg(msg);
+
+        OapiMessageCorpconversationAsyncsendV2Response response;
+
+        try {
+            response = client.execute(request, accessToken);
+        } catch (ApiException e) {
+            logger.error(com.alibaba.fastjson.JSON.toJSONString(request), e);
+            throw new RuntimeException(e.getMessage(), e);
+        }
+
+        if (response == null) {
+            throw new RuntimeException("response is null.");
+        }
+
+        String errCode = response.getErrorCode();
+
+        if (StringUtils.isNotBlank(errCode) && !"0".equals(errCode)) {
+            throw new RuntimeException(response.getErrmsg());
         }
     }
 
