13aa112b6b8f62b2f9a2d9eb3fee2de22a57b0a6
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-05-18 12:55:02 +0800

    upgrade

--- demo-api/src/main/java/com/example/demo/weixin/api/bo/user/MiniUserInfo.java
+++ demo-api/src/main/java/com/example/demo/weixin/api/bo/user/MiniUserInfo.java
@@ -32,4 +32,22 @@ public class MiniUserInfo implements Serializable {
 
     private Watermark         watermark;
 
+    @Getter
+    @Setter
+    public static class Watermark implements Serializable {
+
+        private static final long serialVersionUID = -946609595011819810L;
+
+        /**
+         * 敏感数据归属 appId，开发者可校验此参数与自身 appId 是否一致.
+         */
+        private String            appid;
+
+        /**
+         * 敏感数据获取的时间戳, 开发者可以用于数据时效性校验.
+         */
+        private int               timestamp;
+
+    }
+
 }
