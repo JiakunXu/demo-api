89d472445bc1b1f19f09b9f575b7609b04e296c0
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-05-18 17:24:07 +0800

    upgrade

--- demo-api/src/main/java/com/example/demo/dingtalk/api/bo/AccessToken.java
+++ demo-api/src/main/java/com/example/demo/dingtalk/api/bo/AccessToken.java
@@ -3,12 +3,14 @@ package com.example.demo.dingtalk.api.bo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Getter
 @Setter
 public class AccessToken implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = -3894640525011355121L;
 
     /**
