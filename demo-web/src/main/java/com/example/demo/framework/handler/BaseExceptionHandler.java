cd4ae917c6d84e03101bcc607f56eac8410465d8
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-05-18 18:12:55 +0800

    upgrade

--- demo-web/src/main/java/com/example/demo/framework/handler/BaseExceptionHandler.java
+++ demo-web/src/main/java/com/example/demo/framework/handler/BaseExceptionHandler.java
@@ -18,59 +18,43 @@ public class BaseExceptionHandler {
 
     @ExceptionHandler(value = AccessDeniedException.class)
     public ExceptionResponse exceptionHandler(AccessDeniedException e) {
-        ExceptionResponse response = new ExceptionResponse(Constants.FORBIDDEN, "没有权限，请联系管理员授权");
-
-        return response;
+        return new ExceptionResponse(Constants.FORBIDDEN, "没有权限，请联系管理员授权");
     }
 
     @ExceptionHandler(value = AuthenticationException.class)
     public ExceptionResponse exceptionHandler(AuthenticationException e) {
-        ExceptionResponse response = new ExceptionResponse(Constants.INTERNAL_SERVER_ERROR,
-            e.getMessage());
-
-        return response;
+        return new ExceptionResponse(Constants.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     @ExceptionHandler(value = AsyncRequestTimeoutException.class)
     public ExceptionResponse exceptionHandler(AsyncRequestTimeoutException e) {
-        ExceptionResponse response = new ExceptionResponse(Constants.REQUEST_TIMEOUT,
-            e.getMessage());
-
-        return response;
+        return new ExceptionResponse(Constants.REQUEST_TIMEOUT, e.getMessage());
     }
 
     @ExceptionHandler(value = ServiceException.class)
     public ExceptionResponse exceptionHandler(ServiceException e) {
-        ExceptionResponse response = new ExceptionResponse(e.getCode(), e.getMessage());
-
-        return response;
+        return new ExceptionResponse(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(value = SystemException.class)
     public ExceptionResponse exceptionHandler(SystemException e) {
-        ExceptionResponse response = new ExceptionResponse(e.getCode(), e.getMessage());
-
-        return response;
+        return new ExceptionResponse(e.getCode(), e.getMessage());
     }
 
     @ExceptionHandler(value = RuntimeException.class)
     public ExceptionResponse exceptionHandler(RuntimeException e) {
-        ExceptionResponse response = new ExceptionResponse(Constants.INTERNAL_SERVER_ERROR,
+        return new ExceptionResponse(Constants.INTERNAL_SERVER_ERROR,
             e == null ? "INTERNAL_SERVER_ERROR"
                 : (e.getMessage() != null || e.getCause() == null ? e.getMessage()
                     : e.getCause().getMessage()));
-
-        return response;
     }
 
     @ExceptionHandler(value = Exception.class)
     public ExceptionResponse exceptionHandler(Exception e) {
-        ExceptionResponse response = new ExceptionResponse(Constants.SERVICE_UNAVAILABLE,
+        return new ExceptionResponse(Constants.SERVICE_UNAVAILABLE,
             e == null ? "SERVICE_UNAVAILABLE"
                 : (e.getMessage() != null || e.getCause() == null ? e.getMessage()
                     : e.getCause().getMessage()));
-
-        return response;
     }
 
 }
