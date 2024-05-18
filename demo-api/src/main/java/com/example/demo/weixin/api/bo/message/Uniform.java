cee26fe9123dfadadbf3a8a67824a618e8fd7abb
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-05-18 13:14:56 +0800

    upgrade

--- demo-api/src/main/java/com/example/demo/weixin/api/bo/message/Uniform.java
+++ demo-api/src/main/java/com/example/demo/weixin/api/bo/message/Uniform.java
@@ -96,4 +96,23 @@ public class Uniform implements Serializable {
 
     }
 
+    @Getter
+    @Setter
+    public static class MiniProgram implements Serializable {
+
+        private static final long serialVersionUID = 4742658373201267387L;
+
+        /**
+         * 所需跳转到的小程序appid（该小程序appid必须与发模板消息的公众号是绑定关联关系，暂不支持小游戏）.
+         */
+        private String            appid;
+
+        /**
+         * 所需跳转到小程序的具体页面路径，支持带参数,（示例index?foo=bar），要求该小程序已发布，暂不支持小游戏.
+         */
+        @JSONField(name = "pagepath")
+        private String            pagePath;
+
+    }
+
 }
