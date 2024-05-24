093683768248cba80898cebf99112a6d2e847810
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-05-24 12:06:49 +0800

    upgrade

---
+++ demo-api/src/main/java/com/example/demo/article/api/ArticleService.java
@@ -0,0 +1,4 @@
+package com.example.demo.article.api;
+
+public interface ArticleService {
+}
---
+++ demo-api/src/main/java/com/example/demo/article/api/bo/Article.java
@@ -0,0 +1,16 @@
+package com.example.demo.article.api.bo;
+
+import com.example.demo.framework.bo.BaseBO;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+
+@Getter
+@Setter
+public class Article extends BaseBO {
+
+    @Serial
+    private static final long serialVersionUID = 6414172869018628700L;
+
+}
