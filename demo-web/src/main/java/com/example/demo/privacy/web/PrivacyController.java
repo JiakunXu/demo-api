f991a0cd20084d8b08f3df8a0a2d31008a802f97
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-05-24 14:10:30 +0800

    upgrade

---
+++ demo-web/src/main/java/com/example/demo/privacy/web/PrivacyController.java
@@ -0,0 +1,10 @@
+package com.example.demo.privacy.web;
+
+import com.example.demo.framework.web.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/privacy")
+public class PrivacyController extends BaseController {
+}
