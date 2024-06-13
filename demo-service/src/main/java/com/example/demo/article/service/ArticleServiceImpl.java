b7af22d81c822026d28d0275955515fba96de30a
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-06-13 11:34:22 +0800

    upgrade

--- demo-service/src/main/java/com/example/demo/article/service/ArticleServiceImpl.java
+++ demo-service/src/main/java/com/example/demo/article/service/ArticleServiceImpl.java
@@ -1,8 +1,18 @@
 package com.example.demo.article.service;
 
 import com.example.demo.article.api.ArticleService;
+import com.example.demo.article.dao.mapper.ArticleMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ArticleServiceImpl.class);
+
+    @Autowired
+    private ArticleMapper       articleMapper;
+
 }
--- demo-service/src/main/java/com/example/demo/banner/service/BannerServiceImpl.java
+++ demo-service/src/main/java/com/example/demo/banner/service/BannerServiceImpl.java
@@ -1,8 +1,18 @@
 package com.example.demo.banner.service;
 
 import com.example.demo.banner.api.BannerService;
+import com.example.demo.banner.dao.mapper.BannerMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BannerServiceImpl implements BannerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(BannerServiceImpl.class);
+
+    @Autowired
+    private BannerMapper        bannerMapper;
+
 }
