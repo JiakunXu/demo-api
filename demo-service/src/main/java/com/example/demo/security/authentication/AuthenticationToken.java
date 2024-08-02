88cfd9d89e05c088e0fe1def40db8e93200a4399
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-08-02 10:10:44 +0800

    upgrade

--- demo-service/src/main/java/com/example/demo/security/authentication/AuthenticationToken.java
+++ demo-service/src/main/java/com/example/demo/security/authentication/AuthenticationToken.java
@@ -3,10 +3,15 @@ package com.example.demo.security.authentication;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 
+import java.io.Serial;
 import java.util.Collection;
 
+/**
+ * @author JiakunXu
+ */
 public class AuthenticationToken extends AbstractAuthenticationToken {
 
+    @Serial
     private static final long serialVersionUID = -8610078825675278550L;
 
     private final Object      principal;
