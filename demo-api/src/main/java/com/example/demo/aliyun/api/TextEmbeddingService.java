7938fc36abb33e68227ccd0308301313199aa09f
Jiakun Xu <JiakunXu@users.noreply.github.com>
2024-08-12 09:59:39 +0800

    upgrade

--- demo-api/src/main/java/com/example/demo/aliyun/api/TextEmbeddingService.java
+++ demo-api/src/main/java/com/example/demo/aliyun/api/TextEmbeddingService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public interface TextEmbeddingService {
 
+    void call(String model, String text);
+
     void call(String model, List<String> texts);
 
 }
--- demo-manager/src/main/java/com/example/demo/aliyun/manager/TextEmbeddingServiceImpl.java
+++ demo-manager/src/main/java/com/example/demo/aliyun/manager/TextEmbeddingServiceImpl.java
@@ -15,6 +15,34 @@ import java.util.List;
 public class TextEmbeddingServiceImpl implements TextEmbeddingService {
 
     @Override
+    public void call(String model, String text) {
+        TextEmbeddingParam param = TextEmbeddingParam.builder().apiKey("").model(model).text(text)
+            .build();
+
+        TextEmbedding textEmbedding = new TextEmbedding();
+
+        try {
+            textEmbedding.call(param, new ResultCallback<>() {
+                @Override
+                public void onEvent(TextEmbeddingResult message) {
+                    System.out.println(message);
+                }
+
+                @Override
+                public void onComplete() {
+                }
+
+                @Override
+                public void onError(Exception e) {
+
+                }
+            });
+        } catch (ApiException | NoApiKeyException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    @Override
     public void call(String model, List<String> texts) {
         TextEmbeddingParam param = TextEmbeddingParam.builder().apiKey("").model(model).texts(texts)
             .build();
