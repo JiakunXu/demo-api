85ae732522517ad6b895a96b2b0ad8e383ef4634
Jiakun Xu <JiakunXu@users.noreply.github.com>
2026-04-21 13:41:10 +0800

    Use Lombok @Slf4j and cleanup logging
    Replace manual SLF4J Logger/LoggerFactory usage with Lombok's @Slf4j in ConsumerBroadcastListener and ConsumerClusterListener. Removed explicit logger imports and fields, added @Slf4j annotation, updated log statements to use the generated 'log' instance, and applied minor whitespace/formatting cleanup for injected fields. This reduces boilerplate and standardizes logging behavior.

--- demo-service/src/main/java/com/example/demo/mq/service/ConsumerBroadcastListener.java
+++ demo-service/src/main/java/com/example/demo/mq/service/ConsumerBroadcastListener.java
@@ -1,12 +1,11 @@
 package com.example.demo.mq.service;
 
 import com.example.demo.socket.api.WebSocketService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.MessageModel;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +14,13 @@ import org.springframework.stereotype.Service;
  * @author JiakunXu
  *
  */
+@Slf4j
 @Service
 @RocketMQMessageListener(consumerGroup = "consumer-group", topic = "topic", selectorExpression = "web.socket", messageModel = MessageModel.BROADCASTING, enableMsgTrace = true)
 public class ConsumerBroadcastListener implements RocketMQListener<MessageExt> {
 
-    private static final Logger logger = LoggerFactory.getLogger(ConsumerBroadcastListener.class);
-
     @Autowired
-    private WebSocketService    webSocketService;
+    private WebSocketService webSocketService;
 
     @Override
     public void onMessage(MessageExt message) {
@@ -34,7 +32,7 @@ public class ConsumerBroadcastListener implements RocketMQListener<MessageExt> {
 
                 webSocketService.sendMessage(tunnelId, new String(message.getBody()));
             } catch (Exception e) {
-                logger.error("web.socket", e);
+                log.error("{}", message, e);
             }
         }
     }
--- demo-service/src/main/java/com/example/demo/mq/service/ConsumerClusterListener.java
+++ demo-service/src/main/java/com/example/demo/mq/service/ConsumerClusterListener.java
@@ -5,11 +5,10 @@ import com.example.demo.chat.api.ChatService;
 import com.example.demo.chat.api.bo.Chat;
 import com.example.demo.chat.api.bo.ChatDetail;
 import com.example.demo.subscribe.api.SubscribeService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,17 +17,16 @@ import org.springframework.stereotype.Service;
  * @author JiakunXu
  * 
  */
+@Slf4j
 @Service
 @RocketMQMessageListener(consumerGroup = "consumer-group", topic = "topic", selectorExpression = "chat.message||chat.update", enableMsgTrace = true)
 public class ConsumerClusterListener implements RocketMQListener<MessageExt> {
 
-    private static final Logger logger = LoggerFactory.getLogger(ConsumerClusterListener.class);
+    @Autowired
+    private ChatService      chatService;
 
     @Autowired
-    private ChatService         chatService;
-
-    @Autowired
-    private SubscribeService    subscribeService;
+    private SubscribeService subscribeService;
 
     @Override
     public void onMessage(MessageExt message) {
